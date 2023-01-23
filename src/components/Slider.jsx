16c8440d8c6b3b0f7d92cde057dbe6ddb45754b5
Joseph Gossett <105749663+Blackbolt001@users.noreply.github.com>
2023-01-23 14:15:41 -0500

    adding images

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -6,6 +6,8 @@ const Container = styled.div `
 width:100%;
 height:100vh;
 display:flex;
+position:relative;
+background-color:coral;
 
 `
 const Arrow = styled.div`
@@ -13,14 +15,63 @@ const Arrow = styled.div`
     height:50px;
     background-color: #fff7f7;
     border-radius:50%;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    position:absolute;
+    top:0;
+    bottom:0;
+   left:${(props)=>props.direction === "left" && "10px"};
+   right:${props=>props.direction === "right" && "10px"};
+   cursor:pointer;
+   margin:auto;
+   opacity:0.5;
 `;
+
+const Wrapper = styled.div`
+    height:100%;
+`;
+
+const Slide = styled.div`
+    width:100vw;
+    height:100vh;
+    display:flex;
+    align-items:center;
+`;
+const ImgContainer = styled.div`
+    height:100%;
+    flex:1;
+`;
+const Image = styled.img`
+    height:80%;
+`;
+const InfoContainer = styled.div`
+    flex:1;
+    padding:50px;
+`;
+const Title = styled.h1``;
+const Description = styled.p``;
+const Button = styled.button``;
+
+
+
+
+
 const Slider = () => {
   return (
     <Container>
-        <Arrow>
+        <Arrow direction="left">
             <ArrowLeftOutlined/>
         </Arrow>
-        <Arrow>
+        <Wrapper>
+            <Slide>
+            <ImgContainer>
+           <Image src="https://www.pngplay.com/wp-content/uploads/6/Funny-Donkey-Transparent-PNG.png"/>
+           </ImgContainer>
+            <InfoContainer></InfoContainer>
+            </Slide>
+        </Wrapper>
+        <Arrow direction="right">
             <ArrowRight/>
         </Arrow>
         </Container>
