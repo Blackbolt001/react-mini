9304a64351a627e4cc63b3f876228f325798154a
Joseph Gossett <105749663+Blackbolt001@users.noreply.github.com>
2023-01-29 01:36:23 -0500

    adding cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-//import Home from "./pages/Home";
-//import ProductList from './pages/ProductList';
-import Product from "./pages/Product";
-import Register from "./pages/Register"
+import Cart from "./pages/Cart"
 
 const App = () => {
-    return <Register/>;
+    return <Cart/>;
   };
   
   export default App;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -14,44 +14,37 @@ const Container=styled.div`
      align-items:center;
      justify-content:center;
 `;
-
 const Wrapper=styled.div`
     width:40%;
     padding:20px;
     background-color:white;
 `;
-
 const Title=styled.h1`
     font-size:24px;
     font-weight:300;
 
 `;
-
 const Form=styled.form`
     display:flex;
     flex-wrap:wrap;
 `;
-
 const Input=styled.input`
     flex:1;
     min-width: 40%;
     margin: 20px 10px 0px 0px;
     padding:10px;
 `;
-
 const Agreement=styled.span`
     font-size:12px;
     margin:20px 0px;
 `;
-
 const Button=styled.button`
     width:40%;
     border:none;
     padding:15px 20px;
-    background-color:teal;
-    color:white;
+    background-color: teal;
+    color: white;
     cursor:pointer;
-
 `;
 
 
@@ -73,7 +66,7 @@ const Register = () => {
         </Form>
     </Wrapper>
    </Container>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
