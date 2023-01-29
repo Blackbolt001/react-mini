ff16d1339a85242965800eea2a267264f01bc3c6
Joseph Gossett <105749663+Blackbolt001@users.noreply.github.com>
2023-01-29 02:38:30 -0500

    adding cart items

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import styled from "styled-components"
 import Navbar from "../components/Navbar"
 import Announcement from "../components/Announcement"
@@ -40,7 +39,60 @@ const TopText=styled.span`
     margin: 0px 10px;
 `;
 
-const Bottom=styled.div``
+const Bottom=styled.div`
+    display:flex;
+    justify-content:space-between;
+`;
+
+const Info=styled.div`
+    flex:3;
+`;
+
+const Product=styled.div`
+    display:flex;
+    justify-content:space-between;
+`;
+
+const ProductDetail=styled.div`
+    flex:2;
+`
+
+const Image=styled.img`
+    width:200px;
+    `;
+
+const Details=styled.div`
+    padding:20px;
+    display:flex;
+    flex-direction:column;
+    justify-content:space-around;
+
+`;
+
+const ProductName=styled.span``
+
+const ProductID=styled.span``
+
+const ProductColor=styled.div`
+    width:20px;
+    height:20px;
+    border-radius:50%;
+    background-color:${(props) => props.color};
+
+    `;
+
+const ProductSize=styled.span``
+
+const PriceDetail=styled.span`
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    justify-content:center;
+    `;
+
+const Summary=styled.h1`
+    flex:1;
+`;
 
 
 const Cart = () => {
@@ -52,7 +104,7 @@ const Cart = () => {
             <Title>Your Items</Title>
             <Top>
 
-                <TopButton type="filled">Add More Items</TopButton>
+                <TopButton>Add More Items</TopButton>
                 <TopTexts>
                     <TopText>Shopping Bag(2)</TopText>
                     <TopText>Your WishList</TopText>
@@ -60,7 +112,29 @@ const Cart = () => {
                 <TopButton type="filled">Checkout</TopButton>
 
             </Top>
-            <Bottom></Bottom>
+            <Bottom>
+
+                <Info>
+                    <Product>
+                        <ProductDetail>
+                            <Image src="https://www.pngkey.com/png/detail/124-1245282_sad-dog-png-clipart-royalty-free-stock-sad.png"/>
+                            <Details>
+                            <ProductName><b>Product:</b>HARPERS BAZAAR</ProductName>
+                            <ProductID><b>ID:</b> <b> 01234567890</b></ProductID>
+                            <ProductColor color="black"/>
+                            <ProductSize><b>Size</b><b> 50</b></ProductSize>
+                            </Details>
+                        </ProductDetail>
+                        <ProductDetail>
+                            <PriceDetail>
+                                price
+                            </PriceDetail>
+                        </ProductDetail>
+
+                    </Product>
+                </Info>
+                <Summary>summary</Summary>
+            </Bottom>
         </Wrapper>
         <Footer/>
 
